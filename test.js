05f8e1de0649830b73ad2b526fab61facb012bf8
Jason Knight <Jason@JasonKnight.us>
2022-01-01 17:53:34 -0800

    Try4

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 const { instance } = await WebAssembly.instantiate(
   await Deno.readFile("./bare_metal_wasm.wasm")
 );
@@ -6,5 +7,8 @@ const answer = instance.exports.the_answer();
 console.log(answer);
 
 function handler(_req) {
-  return new Response("Hello, World" + instance.exports.the_answer());
+  return new Response("Hello, World. WASM SIMD computation = " + instance.exports.the_answer());
 }
+
+console.log("Now listening on http://localhost:8000");
+serve(handler);
